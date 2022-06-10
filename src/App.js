d325849715d42428293548e6edc08725c1a31ee3
AlmaJS <alma.bocaj@gmail.com>
2022-06-10 11:28:01 +0100

    updated the CSS file

--- src/App.js
+++ src/App.js
@@ -5,5 +5,67 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState(null);
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "ac4556f22149980b73aed3e08c34243c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div className="Weather">
+        {form}
+        <ul className="Weather">
+          <li>
+            Temperature:
+            {Math.round(weather.temperature)}°C
+          </li>
+          <li>
+            Humidity:
+            {weather.humidity}%
+          </li>
+          <li>
+            Wind:
+            {Math.round(weather.wind)}km/h
+          </li>
+
+          <img src={weather.icon} alt="Weather icon" />
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Weather">
+        <h1>Weather App</h1>
+
+        {form}
+      </div>
+    );
+  }
 }
